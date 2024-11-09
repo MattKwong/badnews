6430e0cd53740f952c49a128df130c1f1de2c542
Matt Kwong <matt@logic.casa>
2024-11-09 07:42:36 -0800

    update blocking css, added block for threads link

--- instagram.js
+++ instagram.js
@@ -14,7 +14,12 @@ function hideNewsfeed() {
     var blockingCss = 
         `
         /* Remove Main Feed - Desktop */
-        main > div > section {
+        [class="x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1"] {
+            display: none !important;
+        }
+
+        /* Remove Friends Reels Bar - Desktop */
+        [class="x1qjc9v5 x78zum5 x1q0g3np xl56j7k xh8yej3"] {
             display: none !important;
         }
 
@@ -24,9 +29,15 @@ function hideNewsfeed() {
         }
 
         /* Remove Reels Link - Desktop */
-        a[href="/reels/videos/"] {
+        a[href="/reels/"] {
             display: none !important;
         }
+
+        /* Remove Threads Link - Desktop */
+        a[href^="https://www.threads.net/"] {
+            display: none !important;
+        }
+
         `;
 
     var style = document.createElement('style');
